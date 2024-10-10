20675ad3416a7e0cb22b49621d7e154e4a1f9cbc
elkomysara <sa1244@fayoum.edu.eg>
2024-10-10 12:07:41 +0300

    Solve Task 0: Const or let

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,6 +1,6 @@
 
 export function taskFirst() {
-  const task = 'I prefer const when I can.';  // Changed to const
+  const task = 'I prefer const when I can.';
   return task;
 }
 
@@ -9,8 +9,8 @@ export function getLast() {
 }
 
 export function taskNext() {
-  let combination = 'But sometimes let';  // Changed to let
-  combination += getLast();  // Combination value changes
+  let combination = 'But sometimes let';
+  combination += getLast();
 
   return combination;
 }
