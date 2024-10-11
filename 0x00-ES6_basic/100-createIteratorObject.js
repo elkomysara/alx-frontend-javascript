11fcf6e9864faafef64720b0025a598becd29277
elkomysara <sa1244@fayoum.edu.eg>
2024-10-11 12:34:42 +0300

    Solve Task 13: Create Iterator Object

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,8 @@
+
+export default function createIteratorObject(report) {
+  let employees = [];
+  for (const department of Object.values(report.allEmployees)) {
+    employees = employees.concat(department);
+  }
+  return employees[Symbol.iterator]();
+}
