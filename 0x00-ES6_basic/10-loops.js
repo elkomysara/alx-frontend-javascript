f8691aeb227e8ae54a016cb19f294758b5c467c3
elkomysara <sa1244@fayoum.edu.eg>
2024-10-11 12:10:34 +0300

    Solve Task 10: Use for...of loop

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+
+export default function appendToEachArrayValue(array, appendString) {
+  for (let value of array) {
+    const idx = array.indexOf(value);
+    array[idx] = appendString + value;
+  }
+
+  return array;
+}
